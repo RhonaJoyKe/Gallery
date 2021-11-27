b5387318948e31f79047edb6a4a283446d8dee37
Moringa School <rhonajoy.koome@student.moringaschool.com>
2021-11-28 00:26:31 +0300

    readds models

--- photos/admin.py
+++ photos/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Image,Category,Location
+
+
+# Register your models here.
+# class ArticleAdmin(admin.ModelAdmin):
+#     filter_horizontal =('tags',)
+
+
+admin.site.register(Image)
+admin.site.register(Category)
+admin.site.register(Location)
