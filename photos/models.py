cda53f88cb8d72a78d704e8e63567f43e2b8787e
Moringa School <rhonajoy.koome@student.moringaschool.com>
2021-11-27 23:05:05 +0300

    adds welcome view method

--- photos/models.py
+++ photos/models.py
@@ -7,8 +7,69 @@ class Image(models.Model):
     description = models.CharField()
     location = models.CharField(max_length = 10,blank =True)
     category=models.CharField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    #saves images
+    def save_image(self):
+        self.save()
+    # delete the image database
+    def delete_image(self):
+        self.delete()
+    # update image
+    def update_image(self, name, description, location, category):
+        self.name = name
+        self.description = description
+        self.location = location
+        self.category = category
+        self.save()
+
+    # get all images
+    @classmethod
+    def get_all_images(cls):
+        images = Image.objects.all()
+        return images
+
+    # get image by id
+    @classmethod
+    def get_image_by_id(cls, id):
+        image = Image.objects.get(id=id)
+        return image
+
+    # get images by location
+    @classmethod
+    def filter_by_location(cls, location):
+        images = Image.objects.filter(location__name=location)
+        return images
+
+    # get images by category
+    @classmethod
+    def filter_by_category(cls, category):
+        images = Image.objects.filter(category__name=category)
+        return images
+
+    # search images
+    @classmethod
+    def search_image(cls, search_term):
+        images = cls.objects.filter(name__icontains=search_term)
+        return images
 class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
+
 class Location(models.Model):
     name = models.CharField(max_length=50, unique=True)
+    # save location to database
+    def save_location(self):
+        self.save()
+
+    # update location
+    def update_location(self, name):
+        self.name = name
+        self.save()
+
+     # delete location from database
+    def delete_location(self):
+        self.delete()
+
+    def __str__(self):
+        return self.name
+
    
--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+import datetime as dt
+from .models import Article
+from django.http  import HttpResponse,Http404
+
 
 # Create your views here.
+def welcome(request):
+    return render(request,'welcome.html')
