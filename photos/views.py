0963e327b1d8a810e0276f9fb1a5dcc2778f4f90
Moringa School <rhonajoy.koome@student.moringaschool.com>
2021-11-28 12:14:30 +0300

    implements the search form

--- photos/views.py
+++ photos/views.py
@@ -8,3 +8,15 @@ from django.http  import HttpResponse,Http404
 def welcome(request):
     photos=Image.get_all_images()
     return render(request,'photos/welcome.html',{'photos':photos})
+def search_results(request):
+
+    if 'name' in request.GET and request.GET["name"]:
+        search_term = request.GET.get("name")
+        searched_articles = Image.search_image(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'photos/search.html',{"message":message,"images": searched_articles})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'all_news/search.html',{"message":message})
